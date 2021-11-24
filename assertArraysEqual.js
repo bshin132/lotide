b9f4e3559a0cccd6e6423abaff36b119c5a53e32
Brian <bshin132@gmail.com>
2021-11-24 03:57:43 +0000

    completed assertArraysEqual exercise

---
+++ assertArraysEqual.js
@@ -0,0 +1,11 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertation Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🆘🆘🆘 Assertation Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+module.exports = assertArraysEqual;
