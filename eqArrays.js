e8c416894c01f54b980d523dfb81b78163b44919
Brian <bshin132@gmail.com>
2021-11-29 22:56:23 +0000

    refactored using module and require

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🆘🆘🆘 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -20,5 +10,4 @@ const eqArrays = function (arr1, arr2) {
   return true;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true); // => should not PASS
+module.exports = eqArrays;
--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertation Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🆘🆘🆘 Assertation Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
 const middle = function(array) {
   if (array.length < 3) {
     return [];
@@ -31,6 +11,4 @@ const middle = function(array) {
   }
 };
 
-
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+module.exports = middle;
